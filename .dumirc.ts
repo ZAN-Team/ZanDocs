2ea311cf7b022f3d2b6b298a81a8ab953901ebc4
yutingzhao1991 <yutingzhao1991@gmail.com>
2023-05-12 10:31:17 +0800

    fix: publicPath

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   favicons: ['https://a.zan.top/static/favicon.png'],
   locales: [{ id: 'en-US', name: 'English' }],
+  runtimePublicPath: {},
+  publicPath: './',
   themeConfig: {
     footer: 'Powered by ZAN',
     logo: 'https://user-images.githubusercontent.com/1061968/237677290-7040df5e-1764-428f-85d7-4b0ff44ee0b3.png',
