81c8d7296b03ef8adb6c485fe3f4e0a29c3c2d8c
yutingzhao1991 <yutingzhao1991@gmail.com>
2023-05-12 11:16:12 +0800

    fix: publicPath

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,14 +2,10 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   // TODO remove base after docs.zan.top ready
-  base: '/ZanDocs',
+  base: '/ZanDocs/',
   favicons: ['https://a.zan.top/static/favicon.png'],
   locales: [{ id: 'en-US', name: 'English' }],
-  runtimePublicPath: {},
-  publicPath:
-    process.env.NODE_ENV === 'production'
-      ? 'https://raw.githubusercontent.com/ZAN-Team/ZanDocs/gh-pages/'
-      : '/',
+  publicPath: '/ZanDocs/',
   themeConfig: {
     footer: 'Powered by ZAN',
     logo: 'https://user-images.githubusercontent.com/1061968/237677290-7040df5e-1764-428f-85d7-4b0ff44ee0b3.png',
