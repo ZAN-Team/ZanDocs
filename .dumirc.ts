71b28968fb24bed46bb33986f1c9f8100bf9afb9
yutingzhao1991 <yutingzhao1991@gmail.com>
2023-05-12 10:48:15 +0800

    fix: publicPath

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,7 +4,10 @@ export default defineConfig({
   favicons: ['https://a.zan.top/static/favicon.png'],
   locales: [{ id: 'en-US', name: 'English' }],
   runtimePublicPath: {},
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? 'https://raw.githubusercontent.com/ZAN-Team/ZanDocs/gh-pages/'
+      : '/',
   themeConfig: {
     footer: 'Powered by ZAN',
     logo: 'https://user-images.githubusercontent.com/1061968/237677290-7040df5e-1764-428f-85d7-4b0ff44ee0b3.png',
